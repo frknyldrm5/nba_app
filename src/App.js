f29831190a206d293c34edc37e5437e8c3d315fa
Furkan Yıldırım <furkanyildirim3455.3455@gmail.com>
2023-11-05 03:39:19 +0100

    navbar and logo added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return (
     <div>
-      <h2>app</h2>
+      <Navbar/>
+        <cwLogo/>
+
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import cwLogo from '../assets/nbalogo.jpeg'
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+        display: "none",
+        fontFamily: "Girassol",
+        [theme.breakpoints.up("sm")]: {
+            display: "block",
+        },
+        "& span": {
+            fontSize: 30,
+            color: "red",
+        },
+    },
+    appBar: {
+        backgroundColor: "yellow",
+    },
+    logo: {
+        width: 50,
+        height: 40,
+    }
+}));
+
+export default function Navbar() {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div className={classes.root}>
+
+            <AppBar position="static" className={classes.appBar}>
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <img src={cwLogo} alt="logo" className={classes.logo}/>
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        -- <span>{"NBA BLOG"}</span>--
+                    </Typography>
+
+                        <div>
+                            <IconButton
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <AccountCircle style={{fontSize: "40px"}} />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={open}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                <MenuItem onClick={handleClose}>My account</MenuItem>
+                            </Menu>
+                        </div>
+
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
