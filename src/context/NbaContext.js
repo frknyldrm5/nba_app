2d19c97391f6bb8728d2913dc0aa594e3688b22d
Furkan Yıldırım <furkanyildirim3455.3455@gmail.com>
2023-11-11 15:54:28 +0100

    context edited

--- src/context/NbaContext.js
+++ src/context/NbaContext.js
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useState } from 'react';
 import axios from 'axios';
+import { addDoc, collection, getDocs, serverTimestamp } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
 
 export const NbaContext = createContext();
 
@@ -28,32 +30,55 @@ const playersOptions = {
 function NbaContextProvider({ children }) {
     const [teamsData, setTeamsData] = useState([]);
     const [playersData, setPlayersData] = useState([]);
+    const [teamsFirestoreData, setTeamsFirestoreData] = useState([]);
+    const [playersFirestoreData, setPlayersFirestoreData] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchRapidAPIData = async () => {
             try {
-                // Fetch teams
+                // Fetch teams from RapidAPI
                 const teamsResponse = await axios.request(teamsOptions);
                 const fetchedTeamsData = teamsResponse.data.response;
-                setTeamsData(fetchedTeamsData);
 
-                // Fetch players for a specific team (replace '1' with the desired team ID)
+                // Fetch players from RapidAPI
                 const playersResponse = await axios.request(playersOptions);
                 const fetchedPlayersData = playersResponse.data.response;
-                setPlayersData(fetchedPlayersData);
 
-                console.log('Teams:', fetchedTeamsData);
-                console.log('Players:', fetchedPlayersData);
+                // Set state for RapidAPI data
+                setTeamsData(fetchedTeamsData);
+                setPlayersData(fetchedPlayersData);
             } catch (error) {
-                console.error(error);
+                console.error('Error fetching RapidAPI data:', error);
             }
         };
 
-        fetchData();
+        const fetchFirebaseData = async () => {
+            try {
+                // Fetch teams from Firestore
+                const teamsFirestoreSnapshot = await getDocs(collection(db, 'teams'));
+                const fetchedTeamsFirestoreData = teamsFirestoreSnapshot.docs.map((doc) => doc.data());
+
+                // Fetch players from Firestore
+                const playersFirestoreSnapshot = await getDocs(collection(db, 'players'));
+                const fetchedPlayersFirestoreData = playersFirestoreSnapshot.docs.map((doc) => doc.data());
+
+                // Set state for Firebase data
+                setTeamsFirestoreData(fetchedTeamsFirestoreData);
+                setPlayersFirestoreData(fetchedPlayersFirestoreData);
+            } catch (error) {
+                console.error('Error fetching Firebase data:', error);
+            }
+        };
+
+        // Fetch data from RapidAPI
+        fetchRapidAPIData();
+
+        // Fetch data from Firebase
+        fetchFirebaseData();
     }, []);
 
     return (
-        <NbaContext.Provider value={{ teamsData, playersData }}>
+        <NbaContext.Provider value={{ teamsData, playersData, teamsFirestoreData, playersFirestoreData }}>
             {children}
         </NbaContext.Provider>
     );
