b394f8812efd7af2e60a9fecdd096467524fcfe7
Furkan Yıldırım <furkanyildirim3455.3455@gmail.com>
2023-11-13 20:41:19 +0100

    main page edited

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,40 +1,25 @@
-import React, { useContext } from 'react';
-import { Card } from 'react-bootstrap';
-import { NbaContext } from '../context/NbaContext';
-import {Link, useNavigate, useParams} from "react-router-dom";
-import Loading from "../components/Loading";
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+import BannerImage from '../assets/lebron.jpeg';
 
+const fontStyle = {
+    fontFamily: 'Arial, sans-serif', // You can replace this with the desired font
+};
 
-function Main(props) {
-    const { teamsFirestoreData } = useContext(NbaContext);
-
-    const navigate = useNavigate();
-    const params = useParams();
-
-
+function Main() {
     return (
-        <div>
-            <h1>Main</h1>
-            <div className="card-container row">
-                {Array.isArray(teamsFirestoreData) ? (
-                    teamsFirestoreData.map((team) => (
-                        <div key={team.id} className="col-md-4 mb-4">
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={team.logo} alt={`${team.nickname} Logo`} />
-                                <Card.Body>
-                                    <Card.Title>{team.name}</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">{team.nickname}</Card.Subtitle>
-                                    <Card.Subtitle className="mb-2 text-muted">{team.city}</Card.Subtitle>
-                                    <Card.Subtitle className="mb-2 text-muted">{team.code}</Card.Subtitle>
-                                    <Link to={`/team-detail/${team.id}`} className="btn btn-primary">View Details</Link>
-                                </Card.Body>
-                            </Card>
-                        </div>
-                    ))
-                ) : (
-                    <Loading/>
-                )}
+        <div className="home" style={{ backgroundImage: `url(${BannerImage})`, backgroundSize: 'cover', minHeight: '100vh' }}>
+            <div className="container h-100">
+                <div className="row h-100 align-items-end">
+                    <div className="col-12 col-md-6 text-light text-md-left mb-4" style={fontStyle}>
+                        <h1 className="display-1 font-weight-bold m-5">NBA</h1>
+                        <p className="lead m-2">Stay informed about the latest developments of the NBA.</p>
+                        <Link to="/teams" className="btn btn-dark btn-lg">
+                            SEE NOW
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     );
