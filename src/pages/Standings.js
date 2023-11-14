d7255563198bfe9534df96a22b9f894aa762e7ef
Furkan Yıldırım <furkanyildirim3455.3455@gmail.com>
2023-11-14 22:48:33 +0100

    standings edited

--- src/pages/Standings.js
+++ src/pages/Standings.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import {NbaContext} from "../context/NbaContext";
+import { NbaContext } from '../context/NbaContext';
 
 function Standings(props) {
     // Access the standings data from the context
@@ -9,23 +9,22 @@ function Standings(props) {
     // Render the standings data
     return (
         <div>
-            <h2>Standings</h2>
+            <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Standings</h2>
 
-            <table>
+            <table style={{ width: '100%', borderCollapse: 'collapse', marginBottom: '20px' }}>
                 <thead>
-                <tr>
-                    <th>Team</th>
-                    <th>Wins</th>
-                    <th>Losses</th>
+                <tr style={{ background: '#f2f2f2' }}>
+                    <th style={{ padding: '10px', textAlign: 'left' }}>Team</th>
+                    <th style={{ padding: '10px', textAlign: 'center' }}>Season</th>
+                    <th style={{ padding: '10px', textAlign: 'center' }}>Streak</th>
                 </tr>
                 </thead>
                 <tbody>
                 {standingsFirestoreData.map((standing) => (
-                    <tr key={standing.id}>
-                        <td>{standing.team.name}</td>
-                        <td>{standing.season}</td>
-                        <td>{standing.streak}</td>
-
+                    <tr key={standing.id} style={{ borderBottom: '1px solid #ddd' }}>
+                        <td style={{ padding: '10px', textAlign: 'left' }}>{standing.team.name}</td>
+                        <td style={{ padding: '10px', textAlign: 'center' }}>{standing.season}</td>
+                        <td style={{ padding: '10px', textAlign: 'center' }}>{standing.streak}</td>
                     </tr>
                 ))}
                 </tbody>
