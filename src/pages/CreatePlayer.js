18ddf0be8a316642c1bbda07ab58383eb47a6341
Furkan Yıldırım <furkanyildirim3455.3455@gmail.com>
2023-11-15 12:45:42 +0100

    create player added

---
+++ src/pages/CreatePlayer.js
@@ -0,0 +1,48 @@
+import {addDoc, collection} from "firebase/firestore";
+import {useNavigate} from "react-router-dom";
+import {useContext, useState} from "react";
+import {NbaContext} from "../context/NbaContext";
+import {db} from "../firebase/firebase";
+import PlayerForm from "../components/PlayerForm";
+
+
+function CreatePlayer() {
+    const playersCollectionRef = collection(db, "players");
+    const [player, setPlayer]  = useState({
+        firstname: "",
+        lastname: "",
+        affiliation: "",
+    });
+    const { playersFirestoreData } = useContext(NbaContext);
+    let navigate = useNavigate();
+
+
+
+    const handleSubmit = async () => {
+        navigate("/");
+
+        await addDoc(playersCollectionRef, {
+            firstname: player.firstname,
+            lastname: player.lastname,
+            affiliation: player.affiliation,
+        });
+    }
+
+
+
+
+    const handleChange = (e) => {
+        setPlayer({...player, [e.target.name]: e.target.value})
+    }
+
+
+
+
+
+    return (
+        <PlayerForm handleChange={handleChange} handleSubmit={handleSubmit}/>
+
+    );
+}
+
+export default CreatePlayer;
