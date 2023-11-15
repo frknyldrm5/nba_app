22790df3893aba6f4935f240cb2c25842ad75111
Furkan Yıldırım <furkanyildirim3455.3455@gmail.com>
2023-11-15 13:55:58 +0100

    env file created

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,13 +5,14 @@ import {getAuth} from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAKoMO3q3FFzaCTQcYjqTb4kAcCrYwu7uc",
-    authDomain: "nba-app-a6edd.firebaseapp.com",
-    projectId: "nba-app-a6edd",
-    storageBucket: "nba-app-a6edd.appspot.com",
-    messagingSenderId: "924176361183",
-    appId: "1:924176361183:web:d31c4cb0993dafbaa2eb6a"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
