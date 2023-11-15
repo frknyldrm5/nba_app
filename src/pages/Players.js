37fdca590467b6126a959681de1ec67dd961281f
Furkan Yıldırım <furkanyildirim3455.3455@gmail.com>
2023-11-15 11:36:37 +0100

    players page updated

--- src/pages/Players.js
+++ src/pages/Players.js
@@ -1,37 +1,52 @@
-import React, { useContext } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Col, Container, Row, Form } from 'react-bootstrap';
 import PlayerCard from '../components/PlayerCard';
 import { NbaContext } from '../context/NbaContext';
-import {useNavigate, useParams} from "react-router-dom";
+import Loading from "../components/Loading";
+import { useNavigate } from 'react-router-dom';
 
 const Players = () => {
-    // Use the useContext hook to access NBA player data from the context
     const { playersFirestoreData } = useContext(NbaContext);
+    const [searchTerm, setSearchTerm] = useState('');
     const navigate = useNavigate();
-    const params = useParams();
-    console.log('Players Data:', playersFirestoreData); // Log the player data to the console
 
+    // Filter players based on the search term
+    const filteredPlayers = playersFirestoreData.filter((player) =>
+        `${player.firstname} ${player.lastname}`.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
-    function detailButtonHandler(player) {
-        navigate(`/player-detail/${player.id}`,{
-            state:{
-                player:player
+    // Handler to navigate to player detail page
+    const detailButtonHandler = (player) => {
+        navigate(`/player-detail/${player.id}`, {
+            state: {
+                player: player
             }
-        })
+        });
     }
 
     return (
         <Container className="mt-3">
             <h2>Players</h2>
+
+            {/* Search bar */}
+            <Form className="mb-3">
+                <Form.Control
+                    type="text"
+                    placeholder="Search by player name"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+            </Form>
+
             <Row>
-                {Array.isArray(playersFirestoreData) ? (
-                    playersFirestoreData.map((player) => (
+                {Array.isArray(filteredPlayers) ? (
+                    filteredPlayers.map((player) => (
                         <Col key={player.id}>
-                            <PlayerCard player={player} detailButtonHandler={detailButtonHandler}/>
+                            <PlayerCard player={player} detailButtonHandler={() => detailButtonHandler(player)} />
                         </Col>
                     ))
                 ) : (
-                    <p>Loading players...</p>
+                    <Loading />
                 )}
             </Row>
         </Container>
